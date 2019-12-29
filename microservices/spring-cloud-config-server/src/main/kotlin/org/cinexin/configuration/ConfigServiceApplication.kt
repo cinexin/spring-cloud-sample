cbfcdde84fdf534ecb27faf72e79766db511c0df
cinexin <migueguillamon@gmail.com>
2019-12-29 14:04:42 +0100

    Configure the configuration server to point to a local URI

--- microservices/spring-cloud-config-server/src/main/kotlin/org/cinexin/configuration/ConfigServiceApplication.kt
+++ microservices/spring-cloud-config-server/src/main/kotlin/org/cinexin/configuration/ConfigServiceApplication.kt
@@ -2,7 +2,9 @@ package org.cinexin.configuration
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
+@EnableConfigServer
 @SpringBootApplication
 open class ConfigServiceApplication
 
