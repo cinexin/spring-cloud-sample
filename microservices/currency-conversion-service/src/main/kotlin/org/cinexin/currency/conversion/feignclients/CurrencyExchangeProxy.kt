53e96e305c5a759b15ddfa034fd3cccb3d293777
cinexin <migueguillamon@gmail.com>
2020-01-25 11:55:34 +0100

    Setting up Zuul Gateway between microservice invocations

--- microservices/currency-conversion-service/src/main/kotlin/org/cinexin/currency/conversion/feignclients/CurrencyExchangeProxy.kt
+++ microservices/currency-conversion-service/src/main/kotlin/org/cinexin/currency/conversion/feignclients/CurrencyExchangeProxy.kt
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable
 /**
  * name = "spring.application.name" in "application.properties" of destination service
  */
-@FeignClient(name="currency-exchange-service")
+@FeignClient(name="netflix-zuul-api-gateway-server")
 @RibbonClient(name="currency-exchange-service")
 interface CurrencyExchangeProxy {
 
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     fun retrieveExchangeValue(@PathVariable("from") from: String,
                               @PathVariable("to") to: String): ExchangeValue?
 }
